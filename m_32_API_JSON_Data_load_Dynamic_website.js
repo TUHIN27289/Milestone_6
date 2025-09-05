3e9d91032f252ff1ec4304a5915100fddc42181f
Toukir <toukerahmed.2728@gmail.com>
2025-09-05 17:09:03 +0600

    32-3 Load Json Data using Fetch Function

--- m_32_API_JSON_Data_load_Dynamic_website.js
+++ m_32_API_JSON_Data_load_Dynamic_website.js
@@ -54,8 +54,43 @@ const person={
     isSubstackUser:false,
     isPatreonUser:false,
 }
+// json=javascript object notation
+// 1. JSON is a data format that is used to store and exchange data.
+// 2. JSON is a lightweight data format.
+// 3. JSON is easy to read and write.
+// 4. JSON is language independent.
+// 5. JSON is a text format that is completely language independent but uses conventions that are familiar to programmers of the C family of languages, including C, C++, C#, Java, JavaScript, Perl, Python, and many others. These properties make JSON an ideal data interchange language.
+
+// JSON Data Types
+// 1. String
+// 2. Number
+// 3. Object
+// 4. Array
+// 5. Boolean
+// 6. Null
+
+// Converting Object to JSON and JSON to Object
+// 1. JSON.stringify() - Converts Object to JSON
+// 2. JSON.parse() - Converts JSON to Object
+
+// Example: 
+
+
+
 console.log(person,'\n\ntyepeof person:',typeof person);
 const personJSON=JSON.stringify(person);//Object converted in to String
 console.log(personJSON,'\n\ntypeof personJSON:',typeof personJSON);// string
 const personParsed=JSON.parse(personJSON);//String converted in to Object
 console.log(personParsed,'\n\ntypeof personParsed:',typeof personParsed); // object
+
+
+//32-3 Load Json Data using Fetch Function
+
+
+
+
+const loadData=()=>{
+     const res=fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response=>response.json())//converting fetched data to json string
+.then(json=>console.log(json));    //printing json string data
+}
